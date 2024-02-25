310e721db445b42d01e6dfeca08f62273d2d0057
GezMin <s.shvetsov.v@gmail.com>
2024-02-26 01:17:37 +0600

    add singIn and SingOut

--- src/components/auth/SingIn.jsx
+++ src/components/auth/SingIn.jsx
@@ -1,18 +1,55 @@
 'use client'
+import { auth } from '@/configFirebase'
+import { signInWithEmailAndPassword } from 'firebase/auth'
 import { useState } from 'react'
 
 const SingIn = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [error, setError] = useState('')
+
+    const logInUser = e => {
+        e.preventDefault()
+
+        signInWithEmailAndPassword(auth, email, password)
+            .then(userCredential => {
+                // Signed in
+                const user = userCredential.user
+                setEmail('')
+                setPassword('')
+                setError('')
+                console.log(user)
+            })
+            .catch(error => {
+                setError(error.message)
+            })
+    }
+
     return (
         <div className='w-full h-[90vh] flex justify-center items-center '>
             <div className='flex flex-col justify-center item-center gap-3 border w-[320px] border-slate-600 p-10 m-3'>
                 <h1 className='text-2xl'>Log In</h1>
-                <input className='p-2' type='email' placeholder='Email' />
-                <input className='p-2' type='password' placeholder='Password' />
-                <button className='p-2 bg-slate-600 text-white hover:bg-slate-700'>
-                    SingIn
+                <input
+                    className='p-2 text-black'
+                    type='email'
+                    placeholder='Email'
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                />
+                <input
+                    className='p-2 text-black'
+                    type='password'
+                    placeholder='Password'
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                />
+                <button
+                    className='p-2 bg-slate-600 text-white hover:bg-slate-700'
+                    onClick={logInUser}
+                >
+                    Log In
                 </button>
+                {error && <p className='text-red-500'>{error}</p>}
             </div>
         </div>
     )
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,25 @@
+'use client'
+import { auth } from '@/configFirebase'
+import { onAuthStateChanged } from 'firebase/auth'
 import Link from 'next/link'
-
+import { useEffect, useState } from 'react'
 const Header = () => {
+    const [authUser, setAuthUser] = useState(null)
+    useEffect(() => {
+        const listener = onAuthStateChanged(auth, user => {
+            if (user) {
+                setAuthUser(user)
+            } else {
+                setAuthUser(null)
+            }
+        })
+        return () => {
+            listener()
+        }
+    }, [])
+
+    console.log(authUser)
+
     return (
         <header className='w-full flex justify-center items-center h-12 bg-slate-600 '>
             <div className='sm:container sm:mx-auto w-full flex justify-between items-center space-x-5 px-3'>
@@ -8,12 +27,33 @@ const Header = () => {
                     YOU LOGO
                 </Link>
                 <div className='flex items-center space-x-5'>
-                    <Link href='/login' className='text-white cursor-pointer'>
-                        Log In
-                    </Link>
-                    <Link href='/singup' className='text-white cursor-pointer'>
-                        Sing Up
-                    </Link>
+                    {authUser ? (
+                        <>
+                            <span>{authUser.email}</span>
+                            <Link
+                                href='/login'
+                                className='text-white cursor-pointer'
+                                onClick={() => auth.signOut()}
+                            >
+                                Log Out
+                            </Link>
+                        </>
+                    ) : (
+                        <Link
+                            href='/login'
+                            className='text-white cursor-pointer'
+                        >
+                            Log In
+                        </Link>
+                    )}
+                    {!authUser ? (
+                        <Link
+                            href='/singup'
+                            className='text-white cursor-pointer'
+                        >
+                            Sing Up
+                        </Link>
+                    ) : null}
                 </div>
             </div>
         </header>
